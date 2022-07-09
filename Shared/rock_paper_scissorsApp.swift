1f470b31e7867bb97e0cc830ccbc1c7e225a3c9b
avniagwl1 <avni.agwl1@gmail.com>
2022-07-09 11:50:18 -0700

    Initial Commit

---
+++ Shared/rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rock_paper_scissorsApp.swift
+//  Shared
+//
+//  Created by Avni Agarwal on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
