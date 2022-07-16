4987295131f858c2ae214337c558bdc4a705058e
avniagwl1 <avni.agwl1@gmail.com>
2022-07-16 11:52:45 -0700

    finished app

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,10 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var score = 0
+    @State var feedback = "click on a buttom to get started!"
+    @State var otherplayer = "all"
+    @State var choices = ["rock","paper","scissors"]
     var body: some View {
-        Text("Rock, Paper, Scissors...")
-            .padding()
+        VStack {
+    Text("Rock, Paper, Scissors")
+    .font(.custom("NoteWorthy-Bold", size:30))
+            Text("score :  \(score)")
+    .font(.custom("NoteWorthy-Bold", size:25))
+            
+      Image(otherplayer)
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .padding()
+    Text(" \(feedback)")
+      .font(.custom("NoteWorthy-Bold", size:25))
+                    
+         
+            HStack {
+        Button(action: {winner(pressed:"rock")}) {
+     Image("rock")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .padding()
+                }
+    Button(action: {winner(pressed:"paper")}) {
+     Image("paper")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .padding()
+                            }
+        Button(action: {winner(pressed:"scissors")}) {
+     Image("scissors")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .padding()
+                            }
+            }
+            
+        }
+        
     }
+ func winner(pressed : String) {
+     otherplayer = choices.randomElement()!
+     
+  if pressed == "rock" {
+      if otherplayer == "rock" {
+          feedback = "it's a tie!"      }
+      if otherplayer == "paper" {
+          feedback = "you lost!"
+      }
+      if otherplayer == "scissors" {
+          feedback = "you won!"
+          score += 1
+        }
+     }
+     if pressed == "scissors" {
+         if otherplayer == "rock" {
+             feedback = "you lost!"  }
+         if otherplayer == "paper" {
+             feedback = "you won!"
+             score += 1
+         }
+         if otherplayer == "scissors" {
+             feedback = "it's a tie!"
+             
+           }
+     }
+     if pressed ==  "paper" {
+         if otherplayer == "rock" {
+             feedback = "you won"
+             score += 1
+         }
+         if otherplayer == "paper" {
+             feedback = "it's a tie!"
+             
+         }
+         if otherplayer == "scissors" {
+             feedback = "you lost!"
+             
+           }
+     }
+ }
 }
 
 struct ContentView_Previews: PreviewProvider {
